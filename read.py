1f98409259f93043535b4ece7aa2ec1d8eb8a128
Justin M DeTone <jdetone@stevens.edu>
2021-02-03 19:39:15 -0500

    Starting to convert the timestamp object of the csv into datetime

--- read.py
+++ read.py
@@ -3,7 +3,20 @@ import os
 print(os.getcwd())
 
 read_file = "data/heart_rate-2021-01-24.json"
+read_file2 = "data/personal_telem.csv"
+read_file3 = "data/distance-2021-01-14.json"
+
 df = pandas.read_json(read_file)
+df_survey = pandas.read_csv(read_file2)
+df3 = pandas.read_json(read_file3)
 
-print(df.head(5))
+print(df.info())
+print(df3.info())
 
+print(df_survey.info())
+
+print(df_survey["Timestamp"].head(5))
+df_survey["Timestamp"] = pandas.to_datetime(df_survey["Timestamp"].head(0), format = "%m/%d/%Y %H:%M:%S")
+print(df_survey["Timestamp"].info())
+
+#likely nan entries causing issues
